c79897cc327a4933f745fac8dd9c72d0932502e2
leonid.mesentsev <leonid.mesentsev@accenture.com>
2023-12-28 12:49:52 +0100

    [FIX] fixes

--- src/services/bleService.ts
+++ src/services/bleService.ts
@@ -14,7 +14,7 @@ enum Characteristics {
 
 const dummyPeripheral: Peripheral = {
   id: "XXX-XXXX-XXXXXXXX",
-  name: "Dummy peripheral",
+  name: "Dummy peripheral (for AppStore)",
   rssi: -1,
   advertising: {},
 };
